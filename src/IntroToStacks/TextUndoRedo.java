1cf1636035b3a212d5914de4cd88495ad3b7241c
League <cole.trammer@gmail.com>
2017-05-01 20:02:26 -0700

    textundoredo

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -7,8 +7,9 @@ import java.util.Stack;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.xml.soap.Text;
 
-public class TextUndoRedo implements KeyListener{
+public class TextUndoRedo implements KeyListener {
 	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -29,35 +30,52 @@ public class TextUndoRedo implements KeyListener{
 	JLabel label;
 	Stack<Character> characters = new Stack<Character>();
 
+	public static void main(String[] args) {
+		TextUndoRedo text = new TextUndoRedo();
+	}
+
 	public TextUndoRedo() {
 		frame = new JFrame();
 		panel = new JPanel();
 		label = new JLabel();
-		frame.add(panel);
 		panel.add(label);
+		frame.add(panel);
+		frame.addKeyListener(this);
 		frame.setVisible(true);
+		frame.setSize(500, 200);
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getKeyCode() == KeyEvent.KEY_PRESSED) {
-			//label.add(comp)
+	
+		if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			String text = label.getText();
+			char lastCharacter = text.charAt(text.length()-1);
+			String leftOverChar = text.substring(0, text.length()-1);
+			characters.push(lastCharacter);
+			label.setText(leftOverChar);
+			System.out.println(lastCharacter);
+			System.out.println(leftOverChar);
+		} else {
+			char key = e.getKeyChar();
+			label.setText(label.getText() + key);
+			System.out.println(key);
 		}
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
-			
-		}
+		// TODO Auto-generated method stub 
+		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
