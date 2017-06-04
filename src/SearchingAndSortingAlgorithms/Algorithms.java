0338ce9156dc965b331d918f69c408fa70fe5ba5
Love4653 <league@iMac-2.attlocal.net>
2017-06-04 16:04:25 -0700

    worn on algorithms

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -1,16 +1,60 @@
 package SearchingAndSortingAlgorithms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
 		for (int i = 0; i < eggs.size(); i++) {
-			if(eggs.get(i).equals("cracked")) {
+			if (eggs.get(i).equals("cracked")) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
-	//Add other methods here
+
+	public static int countPearls(List<Boolean> oysters) {
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i).equals(true)) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		double tallest = 0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > tallest) {
+				tallest = peeps.get(i);
+			}
+		}
+		return tallest;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String longest = "";
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > longest.length()) {
+				longest = words.get(i);
+			}
+
+		}
+		return longest;
+	}
+
+	public static boolean containsSOS(List<String> message1) {
+		for (int i = 0; i < message1.size(); i++) {
+			if (message1.get(i).contains("... --- ...")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+//	public static double sortScores(List<Double> results) {
+//	
+	//}
+	//try to sort the results using the slower way
+	// Add other methods here
 }
