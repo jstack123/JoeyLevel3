267a6ba443a3fce1e4c7f036d51d3b22c37f226a
Eli Smith <league@WTS9.attlocal.net>
2017-06-26 20:01:13 -0700

    exercises with array lists

---
+++ src/com/level3/exercises/LearningArrayLists.java
@@ -0,0 +1,18 @@
+package com.level3.exercises;
+
+import java.util.ArrayList;
+
+public class LearningArrayLists {
+	public static ArrayList<String> createArrayList() {
+		return null;
+	}
+
+	public static ArrayList<String> addToArrayList(ArrayList<String> testList, String testString) {
+		return null;
+	}
+
+	public static int getNumberOfItems(ArrayList<Integer> testList) {
+		return 0;
+	}
+
+}
