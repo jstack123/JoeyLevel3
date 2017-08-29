6d8143dfb648e457d1068bd5725792e33d6c447c
jstack123 <league@WTS19.attlocal.net>
2017-08-28 20:03:01 -0700

    finish arrayList puzzles and start  fibonacci

--- src/com/level3/exercises/ArrayListPuzzles.java
+++ src/com/level3/exercises/ArrayListPuzzles.java
@@ -7,10 +7,15 @@ import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class ArrayListPuzzles implements ActionListener, KeyListener {
 	public static void main(String[] args) {
+		//Number 1
 		/*ArrayList<String> food = new ArrayList<String>();
 		food.add("Steak/pork");
 		food.add("Salads");
@@ -27,21 +32,12 @@ public class ArrayListPuzzles implements ActionListener, KeyListener {
 		System.out.println("You should eat " + food.get(r) + " tonight.");
 */
 		
-		JOptionPane.showMessageDialog(null, "Press space to hear the joke");
-	} 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		
-		
-		ArrayList<String> jokes = new ArrayList<String>();
+		//Number2
+		/*ArrayList<String> jokes = new ArrayList<String>();
 		ArrayList<String> punchlines = new ArrayList<String>();
-
+		
+		
+		
 		jokes.add("Where do animals go when their tail falls off?");
 		jokes.add("What do you call a cow with no legs?");
 		jokes.add("What did the buffalo say to his son when he left for college?");
@@ -59,11 +55,69 @@ public class ArrayListPuzzles implements ActionListener, KeyListener {
 		String r1 = jokes.get(rN);
 		String r2 = punchlines.get(rN);
 		
+		JOptionPane.showMessageDialog(null, r1);
+		JOptionPane.showMessageDialog(null, r2);
+		
+		/*JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		
+		frame.add(panel);
+		panel.add(label);
+		label.setText("Press space to see joke");
+		frame.addKeyListener(null);
+		frame.setSize(200, 200);
+		frame.setVisible(true);
+		*/
+		
+		//Number 3
+		/*ArrayList<String> adjectives = new ArrayList<String>();
+		ArrayList<String> nouns = new ArrayList<String>();
+		adjectives.add("huge ");
+		adjectives.add("tiny ");
+		adjectives.add("aggresive");
+		adjectives.add("ugly ");
+		nouns.add("house");
+		nouns.add("shoes");
+		nouns.add("table");
+		nouns.add("donkey");
+		Random randomAdjectives = new Random();
+		Random randomNouns = new Random();
+		int rA = randomAdjectives.nextInt(adjectives.size());
+		int rN = randomNouns.nextInt(nouns.size());
+		String r3 = adjectives.get(rA);
+		String r4 = nouns.get(rN);
+		JOptionPane.showMessageDialog(null, r3+r4);
+		*/
+		
+		//Number 4
+		/*ArrayList<String> overload = new ArrayList<String>();
+		for (int i = 0; i < 1000000000; i++) {
+			overload.add("hi");
+		}
+		*/
+	} 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode()==KeyEvent.VK_SPACE) {
-			System.out.println("blahblah");
-			JOptionPane.showMessageDialog(null, r1);
-			JOptionPane.showMessageDialog(null, r2);
-		 }
+			System.out.println("efsadf");
+		}
+		
+		
+		
+		
+		
+		//if (e.getKeyCode()==KeyEvent.VK_SPACE) {
+		//	System.out.println("blahblah");
+		//	JOptionPane.showMessageDialog(null, r1);
+			//JOptionPane.showMessageDialog(null, r2);
+		// }
 		
 		
 	}
@@ -79,7 +133,7 @@ public class ArrayListPuzzles implements ActionListener, KeyListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 }
---
+++ src/com/level3/exercises/Fibonnacci.java
@@ -0,0 +1,12 @@
+package com.level3.exercises;
+
+public class Fibonnacci {
+	public static void main(String[] args) {
+		int fib[] = new int[50];
+		fib[0] = 0;
+		fib[1] = 1;
+		for (int i = 2; i < fib.length; i++) {
+			
+		}
+	}
+}
